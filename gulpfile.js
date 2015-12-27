01298cf449042b67be56fb06cc5c6c12bb06798f
Ivan Shen <ivanshen@Ivans-MacBook-Pro.local>
2015-12-27 16:33:26 -0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+
+
+
+gulp.task('styles', function() {
+    gulp.src('./css/*.css')
+        .pipe(gulp.dest('./css'))
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('./css/*.css',['styles']);
+}); 
+
+gulp.task('js', function () {
+    return gulp.src('./js/*.js')
+        // .pipe(concat('all.js'))
+        .pipe(gulp.dest('./js'));
+});
+
+gulp.task('html', function () {
+    return gulp.src('./*.html')
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('js-watch', ['js'], browserSync.reload);
+
+gulp.task('html-watch', ['html'], browserSync.reload);
+
+gulp.task('css-watch', ['css'], browserSync.reload);
+
+gulp.task('serve', ['js', 'html', 'styles'], function () {
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("./*.js", ['js-watch']);
+    gulp.watch("./*.html", ['html-watch']);
+    gulp.watch("./*.css", ['styles']);
+});
---
+++ js/app.js
@@ -0,0 +1,39 @@
+angular.module('babyBuddy', ['ngRoute', 'ui.bootstrap'])
+	.config(['$routeProvider', BBConfig])
+	.controller('BBController', ['$http', '$location', BBController]);
+
+function BBConfig($routeProvider) {
+    $routeProvider.
+		when('/', {
+			templateUrl: 'landing.html'
+		}).
+		when('/months', {
+			templateUrl: 'page2.html',
+			controller: 'BBController'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+};
+function BBController($http, $location) {
+    var vm = this;
+    vm.tabs = ["FIRST","SECOND","THIRD","FOURTH","FIFTH","SIXTH","SEVENTH ","EIGHT","NINTH"];
+    $http.get('pregnancy.json').success(function(data) {
+    	vm.months = data.months;
+    });
+    vm.selectTab = function(id) {
+    	vm.selectedTab = id;
+    	$location.path("/months");
+    };
+    vm.switchTab = function(id) {
+    	vm.selectedTab = id;
+    };
+    vm.goHome = function() {
+    	$location.path("/");
+    }
+    vm.selectedTab = 0;
+    if (window.innerWidth <= 420) {
+        vm.small = true;
+    };
+    
+};
